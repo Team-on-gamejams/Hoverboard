991952bf4f77742c58abbff8aca3b913c7e62ded
Team-on <timonsol13@gmail.com>
2019-11-21 18:53:31 +0200

    Hoberboard animations

--- Hoverboard/Assets/Thirdparty/NVJOB Dynamic Sky Lite SR/Example Scenes/Scripts/AutoСontroller.cs
+++ Hoverboard/Assets/Thirdparty/NVJOB Dynamic Sky Lite SR/Example Scenes/Scripts/AutoСontroller.cs
@@ -10,8 +10,7 @@ public class AutoСontroller : MonoBehaviour
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    public float speed = 1;
-    public Transform camRig;
+    public float speedMove = 1;
     public Transform smog;
 
     //--------------
@@ -40,9 +39,7 @@ public class AutoСontroller : MonoBehaviour
     {
         //--------------
 
-        tr.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
-
-        tr.rotation = Quaternion.Euler(new Vector3((10 - Mathf.PingPong(Time.time, 20)) * 1.5f, tr.eulerAngles.y, 0));
+        tr.Translate(Vector3.right * speedMove * Time.deltaTime, Space.Self);
 
         //--------------
     }
@@ -55,9 +52,8 @@ public class AutoСontroller : MonoBehaviour
     {
         //--------------
 
-        camRig.position = tr.position;        
-
-        if (Physics.Raycast(new Vector3(smog.position.x, smog.position.y + 2, smog.position.z), -tr.up, out hit, 100)) smog.position = new Vector3(smog.position.x, hit.point.y, smog.position.z);
+        if (Physics.Raycast(new Vector3(smog.position.x, smog.position.y + 2, smog.position.z), -tr.up, out hit, 100))
+            smog.position = new Vector3(smog.position.x, hit.point.y, smog.position.z);
 
         //--------------
     }
